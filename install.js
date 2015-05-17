da63abb84e781297631e6e07a92a3e98a3dc5d41
Karthik Vijay <kvijay@localhost.localdomain>
2015-05-17 20:34:15 +0530

    added mkdir to install.js

--- install.js
+++ install.js
@@ -1,5 +1,9 @@
+var sys = require('sys');
+var exec = require('child_process').exec;
 require('crontab').load(function(err,crontab){
 	 var job1 = crontab.create('wallpaper>/tmp/out.txt', '* * * * *');
-	 crontab.save(function(err,crontab){console.log(crontab.jobs())});
+	 crontab.save(function(err,crontab){});
 	 
 });
+
+exec("mkdir ~/Pictures/NatGeo",function(){});
